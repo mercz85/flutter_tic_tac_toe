dad9c071806464d73825c344b842ee229a5c241d
mercz <mercz@hotmail.com>
2022-06-01 19:08:14 +0200

    tic tac toe sin Winner

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 # flutter_tic_tac_toe
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+This project is not mine, is from [A Day Code](https://www.adaycode.com/dev/how-to-draw-tic-tac-toe-on-canvas/ )
+I  just follow the tutorial.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+//todo https://youtu.be/KMdLb5gEOK8?t=622
+enum Mark { X, O, NONE }
+
+const kStrokeWidth = 6.0;
+const kHalfStrokeWidth = kStrokeWidth / 2.0;
+const kDoubleStrokeWidth = kStrokeWidth * 2.0;
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +14,193 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.yellow[50],
+        appBarTheme: const AppBarTheme(
+          color: Colors.blueAccent,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TicTacToe(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TicTacToe extends StatefulWidget {
+  const TicTacToe({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TicTacToe> createState() => _TicTacToeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TicTacToeState extends State<TicTacToe> {
+  Map<int, Mark> _gameMarks = Map();
+  Mark _currentMark = Mark.O;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _addMark(double x, double y) {
+    double _dividedSize = GamePainter.getDividedSize();
+    bool isAbsent = false;
+
+    _gameMarks
+        .putIfAbsent((x ~/ _dividedSize + (y ~/ _dividedSize) * 3).toInt(), () {
+      isAbsent = true;
+      return _currentMark;
+    });
+/*    _gameMarks.putIfAbsent(6, () {
+      isAbsent = true;
+      return _currentMark;
+    });
+    _gameMarks.putIfAbsent(7, () {
+      isAbsent = true;
+      return _currentMark;
+    });
+    _gameMarks.putIfAbsent(8, () {
+      isAbsent = true;
+      return _currentMark;
+    });*/
+
+    if (isAbsent) _currentMark = _currentMark == Mark.O ? Mark.X : Mark.O;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tic Tac Toe'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTapUp: (TapUpDetails details) {
+            setState(() {
+              if (_gameMarks.length >= 9) {
+                _gameMarks = Map<int, Mark>();
+                _currentMark = Mark.O;
+              } else {
+                _addMark(details.localPosition.dx, details.localPosition.dy);
+              }
+            });
+          },
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: Container(
+              child: CustomPaint(
+                painter: GamePainter(_gameMarks),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GamePainter extends CustomPainter {
+  static double _dividedSize = 0;
+  late Map<int, Mark> _gameMarks;
+
+  GamePainter(this._gameMarks);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final blackPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = kStrokeWidth
+      ..color = Colors.black;
+    final blackThickPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = kDoubleStrokeWidth
+      ..color = Colors.black;
+    final redThickPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = kStrokeWidth
+      ..color = Colors.red;
+
+    var x1 = kStrokeWidth;
+    var y1 = _dividedSize - kHalfStrokeWidth;
+    var x2 = size.width - kStrokeWidth;
+    var y2 = _dividedSize - kHalfStrokeWidth;
+    //todo voy por aqui
+    //https://youtu.be/KMdLb5gEOK8?t=366
+    /*
+    print("x1:" + x1.toString() + "y1:" + y1.toString());
+    print("x2:" + x2.toString() + "y2:" + y2.toString());
+    x1: 6.0 y1: 134.14285714285714
+    x2: 405.42857142857144 y2: 134.14285714285714
+    */
+    _dividedSize = size.width / 3.0;
+//drawLine( Offset(x1,y1), Offset(x2,y2), paint)
+    //1st horizontal line
+    canvas.drawLine(
+        Offset(kStrokeWidth, _dividedSize - kHalfStrokeWidth),
+        Offset(size.width - kStrokeWidth, _dividedSize - kHalfStrokeWidth),
+        blackPaint);
+    //2nd horizontal line
+    canvas.drawLine(
+        Offset(kStrokeWidth, _dividedSize * 2 - kHalfStrokeWidth),
+        Offset(size.width - kStrokeWidth, _dividedSize * 2 - kHalfStrokeWidth),
+        blackPaint);
+
+    //1st vertical line
+    canvas.drawLine(
+        Offset(_dividedSize - kHalfStrokeWidth, kStrokeWidth),
+        Offset(_dividedSize - kHalfStrokeWidth, size.height - kStrokeWidth),
+        blackPaint);
+    //2nd vertical line
+    canvas.drawLine(
+        Offset(_dividedSize * 2 - kHalfStrokeWidth, kStrokeWidth),
+        Offset(_dividedSize * 2 - kHalfStrokeWidth, size.height - kStrokeWidth),
+        blackPaint);
+
+    _gameMarks.forEach((index, mark) {
+      switch (mark) {
+        case Mark.O:
+          drawNought(canvas, index, redThickPaint);
+          break;
+        case Mark.X:
+          drawCross(canvas, index, blackThickPaint);
+          break;
+        default:
+          break;
+      }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+  static double getDividedSize() => _dividedSize;
+
+  void drawNought(Canvas canvas, int index, Paint paint) {
+    double left = (index % 3) * _dividedSize + kDoubleStrokeWidth * 2;
+    double top = (index ~/ 3) * _dividedSize + kDoubleStrokeWidth * 2;
+    double noughtSize = _dividedSize - kDoubleStrokeWidth * 4;
+
+    canvas.drawOval(Rect.fromLTWH(left, top, noughtSize, noughtSize), paint);
+  }
+
+  void drawCross(Canvas canvas, int index, Paint paint) {
+    double x1, y1;
+    double x2, y2;
+
+    x1 = (index % 3) * _dividedSize + kDoubleStrokeWidth * 2;
+    y1 = (index ~/ 3) * _dividedSize + kDoubleStrokeWidth * 2;
+
+    x2 = (index % 3 + 1) * _dividedSize - kDoubleStrokeWidth * 2;
+    y2 = (index ~/ 3 + 1) * _dividedSize - kDoubleStrokeWidth * 2;
+
+    canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
+
+    x1 = (index % 3 + 1) * _dividedSize - kDoubleStrokeWidth * 2;
+    y1 = (index ~/ 3) * _dividedSize + kDoubleStrokeWidth * 2;
+
+    x2 = (index % 3) * _dividedSize + kDoubleStrokeWidth * 2;
+    y2 = (index ~/ 3 + 1) * _dividedSize - kDoubleStrokeWidth * 2;
+
+    canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
   }
 }
